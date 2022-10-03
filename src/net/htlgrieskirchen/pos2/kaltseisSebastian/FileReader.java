9930b9b7b69421510d90ccbd3abac808af9c306d
Koitse <kaltseisS190203@sus.htl-grieskirchen.at>
2022-10-03 11:35:47 +0200

    Printable geschafft

--- src/net/htlgrieskirchen/pos2/kaltseisSebastian/FileReader.java
+++ src/net/htlgrieskirchen/pos2/kaltseisSebastian/FileReader.java
@@ -11,16 +11,24 @@ public class FileReader {
     public List<Weapons> weaponsList;
 
     public void readFile() {
-
+        Printable printable = new Printable() {
+            @Override
+            public void print(List<Weapons> weapons) {
+                weapons.forEach(weapons1 -> System.out.println(weapons));
+            }
+        };
         try {
             weaponsList = Files.lines(Path.of("weapons.csv")).skip(1).map(a -> a.split(";")).map(a -> new Weapons(a[0], CombatType.valueOf(a[1]), DamageType.valueOf(a[2]), Integer.parseInt(a[3]), Integer.parseInt(a[4]), Integer.parseInt(a[5]), Integer.parseInt(a[6]))).collect(Collectors.toList());
             weaponsList.sort(Comparator.comparingInt(Weapons::getDamage));
             weaponsList.sort(Comparator.comparing(Weapons::getCombatType));
             weaponsList.sort(Comparator.comparing(Weapons::getDamageType));
             weaponsList.sort(Comparator.comparing(Weapons::getName));
-            for (Weapons weapons : weaponsList) {
-                System.out.println(weapons.toString());
-            }
+
+
+            System.out.println("Printable:");
+            printable.print(weaponsList);
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
